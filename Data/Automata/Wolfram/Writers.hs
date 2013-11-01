b6c90ad260a88e94a949cdac49c6bbec289e42c2
vi <noreply@example.org>
2013-11-02 06:07:34 +0800

    Don't re-export writers from root writer module, name conflicts.

--- Data/Automata/Wolfram/Writers.hs
+++ Data/Automata/Wolfram/Writers.hs
@@ -1,7 +1,1 @@
-module Data.Automata.Wolfram.Writers (
-  module Data.Automata.Wolfram.Writers.Text,
-  module Data.Automata.Wolfram.Writers.Diagram
-) where
-
-import Data.Automata.Wolfram.Writers.Text
-import Data.Automata.Wolfram.Writers.Diagram
+module Data.Automata.Wolfram.Writers where
